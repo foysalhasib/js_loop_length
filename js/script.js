6c95c6d3cc57790e54c6ab77bbf109731c05c9dc
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-02-17 02:48:37 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//============================== string  ==================================
+
+// let Name="Bangladesh is a small country";
+// let len = Name.length;
+
+// for(var i=0; i<len; i++) {
+//     console.log(`index : ${i}`);
+//      console.log(Name[i]);
+// }
+
+
+
+//============================== array  ==================================
+
+
+let food=["apple","cake","iceCrean","Banana"];
+let len = food.length;
+
+for(var i=0; i<len; i++) {
+    console.log(`index : ${i}`);
+     console.log(food[i]);
+}
